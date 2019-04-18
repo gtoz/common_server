950c280ccb3e2599755b7aa00de3d6a05bd8f21e
Administrator <1134133509@qq.com>
2019-04-18 20:12:51 +0800

    菜单栏树构造

--- api/baseApi.js
+++ api/baseApi.js
@@ -5,17 +5,17 @@ var multer = require('multer');
 var UUID = require('uuid')
 var path = require('path')
 
-var storage = multer.diskStorage({  
-    destination: function (req, file, cb) {  
-        cb(null,path.join(__dirname,'../public') )  
-    },  
-    filename: function (req, file, cb) {  
-        
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../public'))
+    },
+    filename: function (req, file, cb) {
+
         var str = file.originalname.split('.');
         cb(null, UUID.v1() + '.' + str[1]);
-    }  
+    }
 })
-var upload = multer({storage: storage})
+var upload = multer({ storage: storage })
 
 
 // var router = express.Router();
@@ -146,22 +146,86 @@ router.post('/action', (req, res) => {
 
 
 
-router.post('/upload',upload.array('file',10),async (req, res, next) => {
-  
-    var sql='insert into resource(filePath,fileName,originalname) values(?,?,?)'
-    let fileIdList=[]
-    for(var i=0;i<req.files.length;i++){
-        var file=req.files[i]
+router.post('/upload', upload.array('file', 10), async (req, res, next) => {
+
+    var sql = 'insert into resource(filePath,fileName,originalname) values(?,?,?)'
+    let fileIdList = []
+    for (var i = 0; i < req.files.length; i++) {
+        var file = req.files[i]
         // goods_picture += req.files[i].filename+'#'
-        var result=await query(sql,[file.path,file.filename,file.originalname])
-       
+        var result = await query(sql, [file.path, file.filename, file.originalname])
+
         fileIdList.push(result.insertId)
-        
+
     }
     jsonWrite(res, fileIdList);
-    
+
 })
 
+router.post('/build', (req, res) => {
+    var p = req.body;
+    var sql = 'SELECT m.* FROM `user` u LEFT JOIN users_roles ur ON ur.user_id=u.id LEFT JOIN role r ON r.id=ur.role_id LEFT JOIN  roles_menus rm ON rm.role_id=r.id LEFT JOIN menu m ON m.id=rm.menu_id WHERE u.id=? '
+    conn.query(sql, [p.id], function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+        jsonWrite(res, buildTree(result, 0, 1));
+    })
+})
+
+function buildTree(result, pid, dep) {
+    var tree = []
+    result.forEach(item => {
+        var children = []
+        if (item.pid == pid) {
+            tree.push(item)
+        }
+    })
+    var d = dep + 1
+
+    tree = tree.map(element => {
+        // console.log(element)
+        var node = {}
+        var children = buildTree(result, element.id, d)
+        if (dep == 1) {
+            if (children.length != 0) {
+                node.redirect = 'noredirect'
+                node.component = 'Layout'
+                node.alwaysShow = true
+                node.meta = {}
+                node.meta.title = element.name
+                node.meta.icon = element.icon
+                node.path = element.path
+                node.name = element.name
+                node.children=children
+            } else {
+                node.component = 'Layout'
+                node.path = element.path
+                var child = {}
+                child.component = element.component
+                child.name = element.name
+                child.path = element.path
+                child.meta = {}
+                child.meta.title = element.name
+                child.meta.icon = element.icon
+                node.children = [child]
+            }
+
+        } else {
+            if (children.length!=0) {
+                node.children = children
+            }
+            node.component = element.component
+            node.meta = {}
+            node.meta.title = element.name
+            node.meta.icon = element.icon
+            node.path = element.path
+            node.name = element.name
+        }
+        return node
+    })
+    return tree
+}
 
 
 
